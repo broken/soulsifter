5cd58436bfaf64d4d6b502ec6d9d5d8498bb0a30
Robby Neale <git@robertneale.com>
2014-08-13 01:30:59 -0700

    groundwork of app

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nodewebkit: {
       options: {
-        version: 'latest', // Version of node-webkit
+        version: '0.10.1', // Version of node-webkit
         buildDir: './build', // Where the build version of my node-webkit app is saved
-        credits: './app/Credits.html', // Mac credits
-        macIcns: './icon.icns', // Path to the Mac icon file
+        credits: './src/credits.html', // Mac credits
+        macIcns: './DVDRipper.icns', // Path to the Mac icon file
         platforms: ['osx'] // These are the platforms that we want to build
       },
-      src: './app/**/*' // Your node-webkit app
+      src: './src/**/*' // Your node-webkit app
     },
   });
 
