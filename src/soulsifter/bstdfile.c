c634cb14c611564cb52021ee33d4ddb3b567ee43
dogatech <dogatech@dogatech.home>
2021-05-22 16:38:51 -0700

    Fix compilation error with bstdfile

--- src/soulsifter/bstdfile.c
+++ src/soulsifter/bstdfile.c
@@ -48,6 +48,7 @@
  ****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include "bstdfile.h"
